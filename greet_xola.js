967329cac9ebab4b4983075fa14d2231d7a91d3f
Moses Moletsane <mosesmoletsane17@gmail.com>
2023-07-17 15:27:28 +0200

    figlet

--- greet_xola.js
+++ greet_xola.js
@@ -12,8 +12,19 @@ You should see Hello, Xola - from ES modules same output as before.
 */
 
 import chalk from 'chalk';
+import figlet from 'figlet';
 //import the greet module that is in the current folder
 import greet from './greet.js'
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
+
+
+figlet(greet('Xola'), function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
